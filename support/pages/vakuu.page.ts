d1e3f561e90795e352773a1f1b6e756da56f8868
Klorkxd <nvanchyk@gmail.com>
2025-02-26 15:04:05 +0300

    Initial Playwright setup with GitHub Actions

---
+++ support/pages/vakuu.page.ts
@@ -0,0 +1,79 @@
+import { Page, Locator, expect } from "@playwright/test"
+export default class Vakuu {
+
+    readonly page: Page;
+    readonly url: string;
+    readonly getStartedButton: Locator;
+    readonly logoOnTopButton: Locator;
+    readonly myAccountButtton: Locator;
+    readonly insuranceLink: Locator;
+    readonly ourPartnerTurvaLink: Locator;
+    readonly calculatePriceButton: Locator;
+    readonly arrowButton: Locator;
+    readonly firstCommentBox: Locator;
+    readonly chooseInsuranceButton: Locator;
+    readonly carInsuranseLink: Locator;
+    readonly blogLink: Locator;
+    readonly rewiewLink: Locator;
+    readonly contactUsLink: Locator;
+    readonly aboutUsLink: Locator;
+    readonly emailInput: Locator;
+    readonly privacyLink: Locator;
+    readonly instagramLink: Locator;
+    readonly twitterLink: Locator;
+    readonly facebookLink: Locator;
+    readonly telegramLink: Locator;
+    readonly homeInsuranceLink: Locator;
+    readonly travelInsuranceLink: Locator;
+    readonly petInsuranceLink: Locator;
+    readonly languageChangeButton: Locator;
+    readonly termsLink: Locator;
+    readonly cookiesLink: Locator;
+    readonly logoBelowButton: Locator;
+    readonly errorText: Locator;
+    readonly developmentText: Locator;
+
+
+
+   constructor(page:Page){
+
+    this.page = page;
+    this.url = 'https://polis812.github.io/vacuu/';
+
+    this.getStartedButton = page.getByText("Get started");
+    this.logoOnTopButton = page.locator('.main__header__logo');
+    this.myAccountButtton = page.locator('.main__header__profile');
+    this.insuranceLink = page.locator('.main__header__item').nth(0);
+    this.calculatePriceButton = page.locator('a[href="/home"]').nth(0);
+    this.arrowButton = page.locator('.arrow-right.arrow-enable');
+    this.firstCommentBox = page.locator('div.review__rating').nth(0);
+    this.chooseInsuranceButton = page.getByText("Choose insurance");
+    this.carInsuranseLink = page.locator('a[href="/car"]');
+    this.blogLink = page.locator('a[href="/blog"]');
+    this.rewiewLink = page.locator('a.main__header__item[href="/review"]');
+    this.contactUsLink = page.locator('a.main__header__item[href="/contact"]')
+    this.aboutUsLink = page.locator('a.footer__col__item[href="/about"]');
+    this.privacyLink = page.locator('a[href="/privacy"]');
+    this.instagramLink = page.locator('.social a img[src*="Instagram"]');
+    this.twitterLink = page.locator('.social a img[src*="Twitter"]')
+    this.facebookLink = page.locator('.social a img[src*="Facebook"]');
+    this.telegramLink = page.locator('.social a img[src*="Telegram"]');
+    this.languageChangeButton = page.locator('.header__lang');
+    this.homeInsuranceLink = page.locator('a.footer__col__item[href="/home"]').nth(0);
+    this.travelInsuranceLink = page.locator('a.footer__col__item[href="/home"]').nth(1);
+    this.petInsuranceLink = page.locator('a.footer__col__item[href="/pet"]');
+    this.cookiesLink = page.locator('a[href="/cookies#"]');
+    this.termsLink = page.locator('a.footer__link[href="#"]')
+    this.logoBelowButton = page.locator('footer .logo img');
+    this.errorText = page.getByText("404");
+    this.developmentText = page.getByText("In development...");
+
+    
+
+ }
+
+async goto() {
+    await this.page.goto(this.url, { waitUntil: "domcontentloaded" });
+}
+
+}
---
+++ tests/vakuu.tests.spec.ts
@@ -0,0 +1,205 @@
+import Vakuu from "../support/pages/vakuu.page";
+const { test, expect } = require('@playwright/test');
+
+test ("Test 1. Redirection using Get Started button", async({page})=>{
+const vakuu = new Vakuu(page);
+await vakuu.goto();
+await vakuu.getStartedButton.click();
+await expect(page.url()).not.toBe('https://polis812.github.io/vacuu/');
+})
+
+test ("Test 2. Reloading the page by clicking to url on top", async({page})=>{
+    const vakuu = new Vakuu(page);
+    await vakuu.goto();
+    await vakuu.logoOnTopButton.click();
+    await page.waitForLoadState('domcontentloaded');
+    await expect(page.url()).toBe('https://polis812.github.io/vacuu/');
+})
+
+test ("Test 3. Redirection to my account page", async ({page})=>{
+    const vakuu = new Vakuu(page);
+    await vakuu.goto();
+    await vakuu.myAccountButtton.click();
+    await expect(page.url()).toBe('https://polis812.github.io/profile');
+    await expect(vakuu.errorText).not.toBeVisible();
+})
+
+test ("Test 4. Redirection to insurance page", async ({page})=>{
+    const vakuu = new Vakuu(page);
+    await vakuu.goto();
+    await vakuu.insuranceLink.click();
+    await expect(page.url()).toBe('https://polis812.github.io/insurance');
+    await expect(vakuu.errorText).not.toBeVisible();
+})
+
+test ("Test 5. Redirection to calculate price page", async ({page})=>{
+    const vakuu = new Vakuu(page);
+    await vakuu.goto();
+    await vakuu.calculatePriceButton.click();
+    await expect(page.url()).toBe('https://polis812.github.io/home');
+    await expect(vakuu.errorText).not.toBeVisible();
+})
+
+test ("Test 6. Usage of arrow button for next comment", async ({page})=>{
+    const vakuu = new Vakuu(page);
+    await vakuu.goto();
+    await expect(vakuu.firstCommentBox).toBeVisible();
+    await vakuu.arrowButton.click();
+    await expect(vakuu.firstCommentBox).not.toBeVisible();
+})
+
+test ("Test 7. Check for updating page after clicking choosing insurance button", async ({page})=>{
+    const vakuu = new Vakuu(page);
+    await vakuu.goto();
+    await vakuu.chooseInsuranceButton.click();
+    await expect(page.url()).toBe('https://polis812.github.io/vacuu/insurance');
+    await expect(vakuu.developmentText).not.toBeVisible();
+})
+
+test ("Test 8. Redirection to car insurance page", async ({page})=>{
+    const vakuu = new Vakuu(page);
+    await vakuu.goto();
+    await vakuu.carInsuranseLink.click();
+    await expect(page.url()).toBe('https://polis812.github.io/car');
+    await expect(vakuu.errorText).not.toBeVisible();
+})
+
+test ("Test 9. Redirection to blog page", async ({page})=>{
+    const vakuu = new Vakuu(page);
+    await vakuu.goto();
+    await vakuu.blogLink.click();
+    await expect(page.url()).toBe('https://polis812.github.io/blog');
+    await expect(vakuu.errorText).not.toBeVisible();
+})
+
+test ("Test 10. Redirection to about us page", async ({page})=>{
+    const vakuu = new Vakuu(page);
+    await vakuu.goto();
+    await vakuu.aboutUsLink.click();
+    await expect(page.url()).toBe('https://polis812.github.io/about');
+    await expect(vakuu.errorText).not.toBeVisible();
+})
+
+test ("Test 11. Redirection to privacy page", async ({page})=>{
+    const vakuu = new Vakuu(page);
+    await vakuu.goto();
+    await vakuu.privacyLink.click();
+    await expect(page.url()).toBe('https://polis812.github.io/privacy');
+    await expect(vakuu.errorText).not.toBeVisible();
+})
+
+test ("Test 12. Redirection to instagram account", async ({page})=>{
+    const vakuu = new Vakuu(page);
+    await vakuu.goto();
+    await vakuu.instagramLink.click();
+    await expect(page.url()).not.toBe('https://polis812.github.io/vacuu/#');
+    await expect(page.url()).toContainText('instagram.com');
+})
+
+test ("Test 13. Changing language to FIN", async ({page})=>{
+    const vakuu = new Vakuu(page);
+    await vakuu.goto();
+    await vakuu.languageChangeButton.click();
+    await vakuu.languageChangeButton.selectOption('FIN');
+    await vakuu.languageChangeButton.click();
+    await expect(vakuu.insuranceLink).not.toContainText("Insurance");
+})
+
+test ("Test 14. Redirection to review page", async ({page})=>{
+    const vakuu = new Vakuu(page);
+    await vakuu.goto();
+    await vakuu.rewiewLink.click();
+    await expect(page.url()).toBe('https://polis812.github.io/review');
+    await expect(vakuu.errorText).not.toBeVisible();
+})
+
+test ("Test 15. Redirection to contact us page", async ({page})=>{
+    const vakuu = new Vakuu(page);
+    await vakuu.goto();
+    await vakuu.contactUsLink.click();
+    await expect(page.url()).toBe('https://polis812.github.io/contact');
+    await expect(vakuu.errorText).not.toBeVisible();
+})
+
+test ("Test 16. Redirection to home insurance page", async ({page})=>{
+    const vakuu = new Vakuu(page);
+    await vakuu.goto();
+    await vakuu.homeInsuranceLink.click();
+    await expect(page.url()).toBe('https://polis812.github.io/home');
+    await expect(vakuu.errorText).not.toBeVisible();
+})
+
+test ("Test 17. Redirection to travel insurance page", async ({page})=>{
+    const vakuu = new Vakuu(page);
+    await vakuu.goto();
+    await vakuu.travelInsuranceLink.click();
+    await expect(page.url()).toBe('https://polis812.github.io/home');
+    await expect(vakuu.errorText).not.toBeVisible();
+})
+
+test ("Test 18. Redirection to pet insurance page", async ({page})=>{
+    const vakuu = new Vakuu(page);
+    await vakuu.goto();
+    await vakuu.petInsuranceLink.click();
+    await expect(page.url()).toBe('https://polis812.github.io/pet');
+    await expect(vakuu.errorText).not.toBeVisible();
+})
+
+
+test ("Test 19. Check correction of main page link", async ({page})=>{
+    const vakuu = new Vakuu(page);
+    await vakuu.goto();
+    await expect(page.url()).toBe('https://polis812.github.io/vakuu/');
+})
+
+test ("Test 20. Redirection to cookies page", async ({page})=>{
+    const vakuu = new Vakuu(page);
+    await vakuu.goto();
+    await vakuu.cookiesLink.click();
+    await expect(page.url()).not.toBe('https://polis812.github.io/#');
+    await expect(vakuu.errorText).not.toBeVisible();
+})
+
+test ("Test 21. Check logo in the footer opens vakuu page", async ({page})=>{
+    const vakuu = new Vakuu(page);
+    await vakuu.goto();
+    await vakuu.logoBelowButton.click();
+    await expect(page.url()).toBe('https://polis812.github.io/vacuu/');
+    await expect(vakuu.logoBelowButton).toBeVisible();
+    await expect(vakuu.insuranceLink).toBeVisible();
+})
+
+test ("Test 22. Redirection to twitter account", async ({page})=>{
+    const vakuu = new Vakuu(page);
+    await vakuu.goto();
+    await vakuu.twitterLink.click();
+    await expect(page.url()).not.toBe('https://polis812.github.io/vacuu/#');
+    await expect(page.url()).toContainText('x.com');
+})
+
+test ("Test 23. Redirection to facebook account", async ({page})=>{
+    const vakuu = new Vakuu(page);
+    await vakuu.goto();
+    await vakuu.facebookLink.click();
+    await expect(page.url()).not.toBe('https://polis812.github.io/vacuu/#');
+    await expect(page.url()).toContainText('facebook.com');
+})
+
+test ("Test 24. Redirection to telegram account", async ({page})=>{
+    const vakuu = new Vakuu(page);
+    await vakuu.goto();
+    await vakuu.telegramLink.click();
+    await expect(page.url()).not.toBe('https://polis812.github.io/vacuu/#');
+    await expect(page.url()).toContainText('telegram.org');
+})
+
+test ("Test 25. Saving selected language on the page after reload", async ({page})=>{
+    const vakuu = new Vakuu(page);
+    await vakuu.goto();
+    await vakuu.languageChangeButton.click();
+    await vakuu.languageChangeButton.selectOption('FIN');
+    await vakuu.languageChangeButton.click();
+    await page.reload();
+    await expect(vakuu.languageChangeButton).toHaveText('FIN');
+})
+
